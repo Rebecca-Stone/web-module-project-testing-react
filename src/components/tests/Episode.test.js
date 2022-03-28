13ff1de423f1b040d2cf77ebb066f476374554cb
Rebecca-Stone <primstone16@gmail.com>
2022-03-28 16:40:18 -0500

    all tests in episode working

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,53 +3,32 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Episode from "./../Episode";
 
+const testEpisode = {
+  id: "",
+  image: null,
+  name: "",
+  season: 1,
+  number: 123,
+  summary: "a summary",
+  runtime: 123,
+};
+
 test("renders without error", async () => {
-  render(
-    <Episode
-      episode={{
-        id: "abc",
-        image: "null",
-        name: "test",
-        season: 2,
-        number: 123,
-        summary: "a summary",
-        runtime: "2",
-      }}
-    />
-  );
-  await screen.queryByText("Chapter One", { exact: false });
+  render(<Episode episode={testEpisode} />);
 });
 
 test("renders the summary test passed as prop", () => {
-  render(
-    <Episode
-      episode={{
-        id: "abc",
-        image: "null",
-        name: "test",
-        season: 2,
-        number: 123,
-        summary: "a summary",
-        runtime: "2",
-      }}
-    />
-  )
-  const episodeSummary = screen.queryByText('a summary', {exact: false})
-  expect(episodeSummary).toBeInTheDocument()
-  expect(episodeSummary).toBeVisible()
+  render(<Episode episode={testEpisode} />);
+  const episodeSummary = screen.queryByText("a summary", { exact: false });
+  expect(episodeSummary).toBeInTheDocument();
+  expect(episodeSummary).toBeVisible();
+  expect(episodeSummary).toHaveTextContent("a summary", { exact: false });
 });
 
-test("renders default image when image is not defined", async () => {
-    render(
-        <Episode episode={{
-            id: "abc",
-        image: "null",
-        name: "test",
-        season: 2,
-        number: 123,
-        summary: "a summary",
-        runtime: "2",
-        }}
-    />)
-    
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisode} />);
+  const episodeImage = screen.queryByAltText(
+    "https://i.ibb.co/2FsfXqM/stranger-things.png"
+  );
+  expect(episodeImage).toBeInTheDocument();
 });
