5cd88b777d7ffeca3237f6053d1c7041c66f03ef
Rebecca-Stone <primstone16@gmail.com>
2022-03-28 18:44:20 -0500

    working with the show test. unsure of what to do

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Show from './../Show';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Show from "./../Show";
 
-test('renders without errors', () => { });
+const testEpisode = {
+  id: "test",
+  image: null,
+  name: "test",
+  season: 5,
+  number: 123,
+  summary: "a summary",
+  runtime: 123,
+};
 
-test('renders Loading component when prop show is null', () => { });
+test("renders without errors", async () => {
+  render(<Show props={testEpisode} />);
+});
 
-test('renders same number of options seasons are passed in', () => { });
+test("renders Loading component when prop show is null", async () => {
+  render(<Show props={null} />);
+  await screen.findByText("Fetching data...", { exact: false });
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test("renders same number of options seasons are passed in", async () => {
+  render(<Show props={testEpisode} handleSelect={Function.prototype} selectedSeason={Function.prototype}/>);
+  const button = screen.findById("seasons", { exact: false });
+  fireEvent.click(button);
+  await screen.findByText("season 5", { exact: false});
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test("handleSelect is called when an season is selected", () => {});
+
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
